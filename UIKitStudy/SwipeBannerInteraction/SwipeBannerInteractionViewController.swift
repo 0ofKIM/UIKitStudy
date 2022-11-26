9b128870cdaa9ac39b4d67d40e7c9fa14c8766c2
uykim3 <uykim3@lotte.net>
2022-11-26 23:51:01 +0900

    swipeBannerInteractionViewController 생성

---
+++ UIKitStudy/SwipeBannerInteraction/SwipeBannerInteractionViewController.swift
@@ -0,0 +1,14 @@
+//
+//  SwipeBannerInteractionViewController.swift
+//  UIKitStudy
+//
+//  Created by 0ofKim on 2022/11/26.
+//
+
+import UIKit
+
+class SwipeBannerInteractionViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
